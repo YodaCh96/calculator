7f2adb690483e6b2c8d736a5610b2e02fc838072
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2021-03-27 15:20:42 +0100

    update the calculator

---
+++ Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    class Calculator
+    {
+
+        int currentState = 0;
+
+        public int Addition(int a, int b)
+        {
+            this.currentState = a + b;
+            return a + b;
+        }
+        public int Subtraktion(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiplikation(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.Write("operation: ");
+                char operation = Console.ReadLine()[0];
+
+                Console.Write("a: ");
+                String operandString1 = Console.ReadLine();
+                int a = Convert.ToInt32(operandString1);
+
+                Console.Write("b: ");
+                String operandString2 = Console.ReadLine();
+                int b = Convert.ToInt32(operandString2);
+
+                Calculator calc = new Calculator();
+
+                int result = 0;
+
+                switch (operation)
+                {
+                    case '*':
+                        result = calc.Multiplikation(a, b);
+                        break;
+                    case '+':
+                        result = calc.Addition(a, b);
+                        break;
+                    case '-':
+                        result = calc.Subtraktion(a, b);
+                        break;
+                    case '/':
+                        result = calc.Division(a, b);
+                        break;
+                }
+
+                Console.WriteLine($"{a} {operation} {b} = {result}");
+            }
+        }
+    }
+}
